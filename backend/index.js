a01877de2fa6eec7d09fec9759df1b4505f9e55f
NavyaJ17 <navyajain170104@gmail.com>
2025-06-27 16:23:30 +0530

    Update frontend origin

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://ai-chatbot-1-wyb4.onrender.com/",
+    origin: "https://ai-chatbot-1-wyb4.onrender.com",
     credentials: true,
   })
 );
